9ece93d40822d088b098aad3f60aadc93718cb07
Min Kim <minkimdom1103@gmail.com>
2023-01-21 00:05:40 -0500

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app color="#111111" dark flat class="px-8">
+    <v-btn icon>
+      <v-icon color="#A7121D">mdi-heart</v-icon>
+    </v-btn>
+
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('Home')">Home</v-btn>
+    <v-btn text @click="scroll('Summary')">Summary</v-btn>
+    <v-btn text @click="scroll('About')">About</v-btn>
+    <v-btn text @click="scroll('Contact')">Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
